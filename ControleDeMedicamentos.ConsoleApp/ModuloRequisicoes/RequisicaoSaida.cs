2f26ffa4e0afa7a42aa9ec7645f6114a0e6ffe4a
anthonidaluz <anthonidaluz@gmail.com>
2026-07-12 16:57:21 -0300

    refactor: adiciona lista e deducao de saidas no estoque do medicamento
    - Atualiza tela de RequisicaoSaida foi adicionado errado
    - Atualiza classe Medicamento para registar requisições de saída.
    - Altera a propriedade QuantidadeEmEstoque para subtrair as saídas das entradas.

--- ControleDeMedicamentos.ConsoleApp/ModuloRequisicoes/RequisicaoSaida.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloRequisicoes/RequisicaoSaida.cs
@@ -1,20 +1,56 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamentos;
+using ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
+using ControleDeMedicamentos.ConsoleApp.Modulos.ModuloPacientes;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloRequisicoes
 {
     public class RequisicaoSaida : EntidadeBase
     {
-        public override void Atualizar(EntidadeBase entidadeAtualizada)
+        public Medicamento Medicamento { get; set; } = null!;
+        public Paciente Paciente { get; set; } = null!;
+        public int Quantidade { get; set; }
+        public DateTime Data { get; set; }
+
+        public RequisicaoSaida() { }
+
+        public RequisicaoSaida(Medicamento medicamento, Paciente paciente, int quantidade, DateTime data) : this()
         {
-            
+            Medicamento = medicamento;
+            Paciente = paciente;
+            Quantidade = quantidade;
+            Data = data;
         }
 
         public override List<string> Validar()
         {
-            throw new NotImplementedException();
+            List<string> erros = new List<string>();
+
+            if (Medicamento == null)
+                erros.Add("O campo \"Medicamento\" é obrigatório.");
+
+            if (Paciente == null)
+                erros.Add("O campo \"Paciente\" é obrigatório.");
+
+            if (Quantidade <= 0)
+                erros.Add("A \"Quantidade\" deve ser maior que zero.");
+
+            if (Data == DateTime.MinValue)
+                erros.Add("A \"Data\" informada é inválida.");
+
+            return erros;
+        }
+
+        public override void Atualizar(EntidadeBase entidadeAtualizada)
+        {
+            RequisicaoSaida requisicaoAtualizada = (RequisicaoSaida)entidadeAtualizada;
+
+            Medicamento = requisicaoAtualizada.Medicamento;
+            Paciente = requisicaoAtualizada.Paciente;
+            Quantidade = requisicaoAtualizada.Quantidade;
+            Data = requisicaoAtualizada.Data;
         }
     }
 }
